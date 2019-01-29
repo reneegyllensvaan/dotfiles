5f191a3efd0cd58a414ae3057bdcdf402418a947
Renee Gyllensvaan <rgyllensvaan@bison.co>
2019-01-29 18:02:49 -0500

    template the readme

---
+++ README.md.tmpl.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cat << EOF
+# Renee's dotfiles
+right now i'm working on these dotfiles to get them where i want them, i have kind of a long-term spare time project to make sure these work as a regular getting-started set of dotfiles
+
+# Minimum dependencies on OSX
+`cat system-packages/osx/minimum`
+
+# Recommended dependencies on OSX
+`cat system-packages/osx/recommended`
+EOF
