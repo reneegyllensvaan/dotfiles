f018428cc4b52eddb57b6fed8cb9b7000832cf74
Renee Gyllensvaan <renee.gyllensvaan@gmail.com>
2020-10-04 23:02:34 +0200

    also get rid of nvim configs

--- nvim/globals.lua
+++
@@ -1,5 +0,0 @@
--- Global functions and definitions for lua
-HOMEDIR = os.getenv('HOME')
-STATE_FILE_DIR = HOMEDIR..'/.local/share/nvim/'
-PLUGIN_DIRECTORY = STATE_FILE_DIR..'/plugged'
-
--- nvim/hi.vim
+++
@@ -1,4 +0,0 @@
-hi WhichKey ctermbg=none
-hi WhichKeyDesc ctermbg=none
-hi WhichKeyName ctermbg=none
-hi Pmenu ctermbg=black ctermfg=white
