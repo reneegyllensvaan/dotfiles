4f6cb258848e058fa958fb8eddf1a53206a31742
Renee Gyllensvaan <rgyllensvaan@cobaltgp.com>
2020-10-05 08:17:47 +0000

    fix the setup i broke

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Renee's dotfiles
-right now i'm working on these dotfiles to get them where i want them, i have kind of a long-term spare time project to make sure these work as a regular getting-started set of dotfiles
-
-# Minimum dependencies on OSX
-
-
-# Recommended dependencies on OSX
-
+i write these so i can write the code
 
--- README.md.tmpl.sh
+++ README.md.tmpl.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 cat << EOF
 # Renee's dotfiles
-right now i'm working on these dotfiles to get them where i want them, i have kind of a long-term spare time project to make sure these work as a regular getting-started set of dotfiles
-
-# Minimum dependencies on OSX
-`cat system-packages/osx/minimum`
-
-# Recommended dependencies on OSX
-`cat system-packages/osx/recommended`
+i write these so i can write the code
 
 EOF
 exit 0
