7660dc2a41121e8ceabe8cdd83fc3e627766a157
Renee Gyllensvaan <rgyllensvaan@cobaltgp.com>
2022-07-08 09:26:09 +0000

    Make git prompt show file counts per status

--- shell/zfunctions/git-prompt.zsh
+++ shell/zfunctions/git-prompt.zsh
@@ -1,18 +1,60 @@
+function count_expr() {
+  local staged_changed_count=$(echo $2 | grep -E $1 | wc -l)
+  echo "${staged_changed_count// /}"
+}
+
 function parse_git_dirty() {
   local STATUS=''
   local -a FLAGS
   FLAGS=('--porcelain')
-  STATUS=$(command git status ${FLAGS} 2> /dev/null | tail -n1)
-  if [[ -n $STATUS ]]; then
-    echo "*"
-  else
-    echo ""
+  STATUS=$(command git status --porcelain 2>/dev/null | sed -E 's/^(..).*/\1/')
+  if [[ ! -n $STATUS ]]; then
+    return
   fi
+  out=()
+
+  local staged_modified_count=$(count_expr '^M' "$STATUS")
+  if [[ "$staged_modified_count" != 0 ]]; then
+    out+="%{$fg[green]%}$staged_modified_count+"
+  fi
+
+  local staged_added_count=$(count_expr '^A' "$STATUS")
+  if [[ "$staged_added_count" != 0 ]]; then
+    out+="%{$fg[green]%}$staged_added_count++"
+  fi
+
+  local staged_deleted_count=$(count_expr '^D' "$STATUS")
+  if [[ "$staged_deleted_count" != 0 ]]; then
+    out+="%{$fg[green]%}${staged_deleted_count}--"
+  fi
+
+  local unstaged_modified_count=$(count_expr '^.M' "$STATUS")
+  if [[ "$unstaged_modified_count" != 0 ]]; then
+    out+="%{$fg[red]%}$unstaged_modified_count+"
+  fi
+
+  # Not sure if this actually happens, I think these are just untracked?
+  local unstaged_added_count=$(count_expr '^.A' "$STATUS")
+  if [[ "$unstaged_added_count" != 0 ]]; then
+    out+="%{$fg[red]%}$unstaged_added_count++"
+  fi
+
+  local unstaged_deleted_count=$(count_expr '^.D' "$STATUS")
+  if [[ "$unstaged_deleted_count" != 0 ]]; then
+    out+="%{$fg[red]%}${unstaged_deleted_count}--"
+  fi
+
+  local untracked_count=$(count_expr '^\?\?' "$STATUS")
+  if [[ "$untracked_count" != 0 ]]; then
+    out+="%{$fg[red]%}${untracked_count}?"
+  fi
+
+  echo " %{$reset_color%}(${out}%{$reset_color%})"
 }
 
 function git_prompt_info() {
   local ref
   ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
-  echo -n "%{$fg[green]%}[${ref#refs/heads/}%{$fg[red]%}$(parse_git_dirty)%{$reset_color%}%{$fg[green]%}]%{$reset_color%} "
+  echo -n "%{$fg[green]%}[${ref#refs/heads/}$(parse_git_dirty)%{$fg[green]%}]%{$reset_color%} "
 }
