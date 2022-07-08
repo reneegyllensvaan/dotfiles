9e15297832242160a6adbbbd35008ff58de00f35
Renee Gyllensvaan <rgyllensvaan@cobaltgp.com>
2022-07-08 09:25:57 +0000

    Clean up dir-marks

--- shell/zfunctions/dir-marks.zsh
+++ shell/zfunctions/dir-marks.zsh
@@ -67,34 +67,3 @@ mark-dir () {
     mv "$DIR_MARK_PATH"'.tmp' "$DIR_MARK_PATH"
   fi
 }
-
-# Let's make a completion!
-#compdef _pop-mark pop-mark
-function _pop-mark {
-    local line
-
-    _arguments -C \
-        "-h[Show help information]" \
-        "--h[Show help information]" \
-        "1: :(quietly loudly)" \
-        "*::arg:->args"
-
-    case $line[1] in
-        loudly)
-            _hello_loudly
-        ;;
-        quietly)
-            _hello_quietly
-        ;;
-    esac
-}
-
-function _hello_quietly {
-    _arguments \
-        "--silent[Dont output anything]"
-}
-
-function _hello_loudly {
-    _arguments \
-        "--repeat=[Repat the <message> any number of times]"
-}
